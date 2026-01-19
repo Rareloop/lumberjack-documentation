04e1c80143ed9a54890f9ebd51e87d16fe62d52a
Tom Mitchelmore <tom.mitchelmore@rareloop.com>
2026-01-19 09:33:13 +0000

    Update social image

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -68,7 +68,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/thumbnail.jpg",
     colorMode: {
       respectPrefersColorScheme: false,
       disableSwitch: true,
